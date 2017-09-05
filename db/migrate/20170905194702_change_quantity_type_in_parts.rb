1aea6fd36c6d4e77d37d8a9ea3a663c7bee59f78
tenzin2017 <tenzinleo@gmail.com>
2017-09-05 16:01:43 -0400

    change column is implemented on quantity type from intger to decimal

---
+++ db/migrate/20170905194702_change_quantity_type_in_parts.rb
@@ -0,0 +1,9 @@
+class ChangeQuantityTypeInParts < ActiveRecord::Migration[5.0]
+  def self.up
+    change_column :parts, :quantity, :decimal
+  end
+
+  def self.down
+    change_column :parts, :quantity, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905193044) do
+ActiveRecord::Schema.define(version: 20170905194702) do
 
   create_table "locations", force: :cascade do |t|
     t.string "name"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20170905193044) do
     t.string  "name"
     t.string  "manufacturer"
     t.decimal "cost"
-    t.integer "quantity"
+    t.decimal "quantity"
   end
 
   create_table "widgets", force: :cascade do |t|
